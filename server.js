28ac61a6cbe3b3914b4dc76d86911f13b4e826e5
Marta <Marta Fonda>
2014-10-10 13:01:51 +0200

    Changing HTML response

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var http = require('http');//importamos modulo http de node
 
 //Callback
 http.createServer(function (request, response) { //Creo servidor
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hola Mundo\n');
+  response.write('Hola, mundo\n');
+  response.write('Adios, mundo');
+  response.end();
 }).listen(1337, '127.0.0.1'); //Le digo que se ponga a escuchar por un puerto
 
 console.log('Server running at http://127.0.0.1:1337/');
