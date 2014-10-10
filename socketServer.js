6c8c2023a1fc73cbd969121203367dcbbd4af752
Marta <Marta Fonda>
2014-10-10 13:54:57 +0200

    creating shocket server

---
+++ socketServer.js
@@ -0,0 +1,16 @@
+var net = require('net');
+var port = 1702;
+var server = net.createServer(function(connection) {
+  console.log('Connection open');
+  connection.write('Hello?\r\n');
+  connection.on('data', function(data) {
+    if (String(data).trim() != 'hello') {
+        connection.write('ERROR\r\n');
+    } else {
+        connection.end('world\r\n');
+        console.log('connection closed');
+    }
+  });
+});
+server.listen(port);
+console.log("Socket server listening on port %s", port);
