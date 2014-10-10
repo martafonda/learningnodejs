c17cd9367f806e711775ba13ea729dc78cca00e0
Marta <Marta Fonda>
2014-10-10 13:57:05 +0200

    Allow upper and lower case

--- socketServer.js
+++ socketServer.js
@@ -4,7 +4,7 @@ var server = net.createServer(function(connection) {
   console.log('Connection open');
   connection.write('Hello?\r\n');
   connection.on('data', function(data) {
-    if (String(data).trim() != 'hello') {
+    if (String(data).trim().toLowerCase() != 'hello') {
         connection.write('ERROR\r\n');
     } else {
         connection.end('world\r\n');
