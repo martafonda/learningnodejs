9231305f3535ae174adc20b85f2076ab01c5cc62
Marta <Marta Fonda>
2014-10-10 13:21:56 +0200

    Handle 500 error if is not my url

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var http = require('http');//importamos modulo http de node
-
 //Callback
 http.createServer(function (request, response) { //Creo servidor
-  response.write('Hola, mundo\n');
-  response.write('Adios, mundo');
-  response.end();
+	if(request.url != '/'){
+		response.writeHead(500, {'Content-Type': 'text/plain'});
+		response.end('ERROR 500\n');
+	}else{
+		response.write('Hola, mundo\n');
+		response.write('Adios, mundo');	
+		response.end();
+	}
 }).listen(1337, '127.0.0.1'); //Le digo que se ponga a escuchar por un puerto
 
 console.log('Server running at http://127.0.0.1:1337/');
